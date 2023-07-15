226d485bcad34df6c3408cab3ca2ed0b6970f242
unknown <serranasoler@gmail.com>
2023-07-15 13:14:34 -0300

    navbarfooter

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+
+app.listen (3000, () =>
+console.log ("Servidor corriendo en el puerto 3000"));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+res.sendFile(path.join(__dirname, './views/navfooter.html'));
+});
