db75ca0e3c2fe545affc7a0436877a20629c0c15
unknown <serranasoler@gmail.com>
2023-07-15 17:40:32 -0300

    login

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express = require ('express');
+const express = require('express');
 const path = require('path');
 const app = express();
 
-app.listen (3000, () =>
-console.log ("Servidor corriendo en el puerto 3000"));
+app.listen(3000, () =>
+    console.log("Servidor corriendo en el puerto 3000"));
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, './views/navfooter.html'));
+    res.sendFile(path.join(__dirname, './views/navfooter.html'));
 });
 
 app.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, './views/registro.html'));
-    });
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
