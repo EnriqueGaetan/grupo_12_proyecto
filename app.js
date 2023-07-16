25cc4ef13eb5d90cbe4d4cd83daf9b52dcbb495e
lmbanegas <lbanegas93@gmail.com>
2023-07-16 12:21:35 -0300

    -

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.use(express.static('public'));
+
+const publicPath = path.join(__dirname,'public');
+
 app.listen(3000, () =>
     console.log("Servidor corriendo en el puerto 3000"));
 
@@ -21,4 +25,8 @@ app.get('/login', (req, res) => {
 
 app.get('/carritodecompras', (req, res) => {
     res.sendFile(path.join(__dirname, './views/carritocompras.html'));
+});
+
+app.get('/detalleproducto', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productdetail.html'));
 });
