a73de9dff31282ed997fb83e185bdfb5707be11c
lmbanegas <lbanegas93@gmail.com>
2023-10-12 19:20:05 -0300

    Sprint 6

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,8 +1,24 @@
+const { log } = require('console');
+const { User } = require('../database/models');
+const { Op } = require('sequelize');
+
 const controllers = {
     home: (req, res) => {
         res.render('home', { user: req.session.user });
     },
+    controlpanel: async (req, res) => {
+        try {
+            const allUsers = await User.findAll({ raw: true });
+
+
+            res.render('controlpanel', { allUsers });
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
+
 
 
     
